f7fb9428309e5e4634a3bb289fecf1462abdc88b
LeandroFreireGH <leandro.freire@ucuenca.edu.ec>
2025-05-26 21:23:46 -0500

    LF-Base structure

--- View/beginForm.php
+++ View/beginForm.php
@@ -1,4 +1,11 @@
-<p>Hola</p>
-<?php
-echo "Hola que haces";
-<?php>
+<html>
+<head>
+<title>Listing 9.6 An HTML form that calls itself</title>
+</head>
+<body>
+    <form action="<?php print $_SERVER[PHP_SELF] ?>" method="POST">
+    Type your guess here: <input type="text" name="guess">
+    <?php print $_SERVER[PHP_SELF] ?>
+    </form>
+</body>
+</html>
